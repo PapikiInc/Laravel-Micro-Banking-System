6a42591305c10d3948d7216990744498be28c096
Sone <Sone@incendiaryblue.com>
2023-05-28 20:29:32 +0200

    migration and seeding works

--- database/migrations/2023_05_28_1614_create_account_transactions_table.php
+++ database/migrations/2023_05_28_1614_create_account_transactions_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -31,4 +30,3 @@ class CreateAccountTransactionsTable extends Migration
         Schema::dropIfExists('account_transactions');
     }
 }
-
